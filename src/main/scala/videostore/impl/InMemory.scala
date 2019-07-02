b5d45a4518c6592cba18677dc3b1ae3df52db18b
Dragisa Krsmanovic <dragishak@gmail.com>
2019-07-01 21:15:30 -0700

    concurrent maps

--- src/main/scala/videostore/impl/InMemory.scala
+++ src/main/scala/videostore/impl/InMemory.scala
@@ -1,42 +1,48 @@
 package videostore.impl
 
 import java.util.UUID
+import java.util.concurrent.{ConcurrentHashMap, ConcurrentMap}
 
 import videostore.{DVD, ErrorOr, Movie}
 import videostore.VideoRental._
+import scala.collection.JavaConverters._
 
 object InMemory {
 
-  private var movies: Map[Movie, Set[DVD]] = Map()
-  private var availableDVDs: Set[DVD]      = Set()
-  private var rentedDVDs: Set[DVD]         = Set()
+  private val movies: ConcurrentMap[Movie, Set[DVD]]  = new ConcurrentHashMap[Movie, Set[DVD]]() {}
+  private val availableDVDs: ConcurrentMap[DVD, Unit] = new ConcurrentHashMap[DVD, Unit]()
+  private val rentedDVDs: ConcurrentMap[DVD, Unit]    = new ConcurrentHashMap[DVD, Unit]()
 
   val interpreter: Interpreter[ErrorOr] = new Interpreter[ErrorOr] {
 
     override def apply[A](fa: DSL[A]): ErrorOr[A] = fa match {
 
       case AddInventory(movie, qty) =>
-        val dvds = List.fill(qty)(UUID.randomUUID()).toSet
-        movies += movie -> (movies.getOrElse(movie, Set()) ++ dvds)
-        availableDVDs ++= dvds
-        Right(dvds)
+        val dvds = List.fill(qty)(UUID.randomUUID())
+
+        val current = movies.getOrDefault(movie, Set[DVD]()) ++ dvds
+
+        movies.put(movie, current)
+
+        availableDVDs.putAll(dvds.map((_, ())).toMap.asJava)
+        Right(dvds.toSet)
 
       case SearchForDVD(movie) =>
-        Right(movies.get(movie).flatMap(_.headOption))
+        Right(Option(movies.get(movie)).flatMap(_.headOption))
 
       case RentDVD(dvd) =>
-        if (availableDVDs.contains(dvd)) {
-          availableDVDs -= dvd
-          rentedDVDs += dvd
+        if (availableDVDs.containsKey(dvd)) {
+          availableDVDs.remove(dvd)
+          rentedDVDs.put(dvd, ())
           Right(())
         } else {
           Left(s"$dvd is not available")
         }
 
       case ReturnDVD(dvd) =>
-        if (rentedDVDs.contains(dvd)) {
-          rentedDVDs -= dvd
-          availableDVDs += dvd
+        if (rentedDVDs.containsKey(dvd)) {
+          rentedDVDs.remove(dvd)
+          availableDVDs.put(dvd, ())
           Right(())
         } else {
           Left(s"$dvd has not been rented")
