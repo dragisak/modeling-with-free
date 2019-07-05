dba2fbfb9828ce78aca196f8deca48ea3cbdb5be
Dragisa Krsmanovic <dragishak@gmail.com>
2019-07-04 20:48:46 -0700

    Test passing

--- src/test/scala/videostore/freeimpl/FreeTests.scala
+++ src/test/scala/videostore/freeimpl/FreeTests.scala
@@ -3,7 +3,7 @@ package videostore.freeimpl
 import cats.arrow.FunctionK
 import cats.free._
 import cats.implicits._
-import cats.{Eq, InjectK}
+import cats.{Eq, InjectK, ~>}
 import videostore.ErrorOr
 import videostore.freeimpl.VideoRentalFree.DSL
 
@@ -12,13 +12,16 @@ trait FreeTests {
   def interpreter: VideoRentalFree.Interpreter[ErrorOr]
 
   implicit val inject: InjectK[DSL, ErrorOr] = new InjectK[DSL, ErrorOr] {
-    override def inj: FunctionK[DSL, ErrorOr] = interpreter
+    override def inj: DSL ~> ErrorOr = interpreter
 
-    override def prj: FunctionK[ErrorOr, Lambda[A => Option[DSL[A]]]] =
-      λ[FunctionK[ErrorOr, Lambda[A => Option[DSL[A]]]]] {
-        case Left(err) => err
-        case Right(a)  => None
+    type Opt[A] = Option[DSL[A]]
+
+    override def prj: ErrorOr ~> Opt = new (ErrorOr ~> Opt) {
+      override def apply[A](fa: ErrorOr[A]): Option[DSL[A]] = fa match {
+        case Left(err) => None // ??
+        case Right(a)  => None // ??
       }
+    }
   }
 
   implicit def freeDSLEq[A: Eq]: Eq[Free[DSL, A]] =
